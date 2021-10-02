87ec674aae29e63fb98467be9940987b839542e2
Adam Friedman <adam.friedman5@gmail.com>
2021-10-02 11:47:58 -0700

    Create Trails Component

--- src/components/Trails/Trails.js
+++ src/components/Trails/Trails.js
@@ -1,3 +1,13 @@
+import { Typography } from "@material-ui/core";
+import TrailCard from "../Cards/TrailCard";
+
 export default function Trails() {
-  return <>Trails</>;
+  return (
+    <>
+      <Typography component={"h2"} variant={"h2"}>
+        Trails
+      </Typography>
+      <TrailCard />
+    </>
+  );
 }
