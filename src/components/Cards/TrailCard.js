87b86520d35c7a1421c56677e4b8f666870bf943
Adam Friedman <adam.friedman5@gmail.com>
2021-10-02 11:47:39 -0700

    Create TrailCard component

---
+++ src/components/Cards/TrailCard.js
@@ -0,0 +1,74 @@
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    businessContentCard: {
+      color: theme.palette.primary.main,
+      width: 340,
+      margin: theme.spacing(1),
+      [theme.breakpoints.down("xs")]: {
+        width: "100%",
+        maxWidth: 340,
+        margin: 0,
+        marginTop: theme.spacing(2),
+        "&:nth-child(1)": {
+          marginTop: 0,
+        },
+      },
+    },
+    cardContent: {
+      paddingTop: theme.spacing(1.25),
+    },
+    cardActions: {
+      display: "flex",
+      padding: theme.spacing(1),
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    image: {
+      width: "100",
+      height: 250,
+      border: "1px solid blue",
+    },
+  })
+);
+
+export default function TrailCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.businessContentCard} data-testid="businessCard">
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="flex-start"
+        padding={2}
+        bgcolor={"lightgrey"}
+      >
+        Trail Title
+      </Box>
+
+      <CardContent className={classes.cardContent}>
+        <Box className={classes.image}>Image</Box>
+
+        <Typography variant="body1" component="p">
+          Description
+        </Typography>
+      </CardContent>
+
+      <Divider />
+
+      <CardActionArea className={classes.cardActions}>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          Button
+        </Box>
+      </CardActionArea>
+    </Card>
+  );
+}
