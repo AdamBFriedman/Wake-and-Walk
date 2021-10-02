75fe79c4e23dd310bac814db09d57e4067a50d91
Adam Friedman <adam.friedman5@gmail.com>
2021-10-02 11:24:21 -0700

    Add Trails component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "../src/images/logo.png";
 import "./App.css";
+import Trails from "./components/Trails/Trails";
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <Trails />
     </div>
   );
 }
